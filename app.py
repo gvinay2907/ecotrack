43f9fda79c35e1f1a761697f7512cf88cdab4430
gvinay2907 <vinayyyyy007@gmail.com>
2026-03-10 17:54:01 +0530

    ecotrack project

--- app.py
+++ app.py
@@ -957,4 +957,12 @@ def logout():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
